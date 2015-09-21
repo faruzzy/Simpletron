f5e12d5b6f076af8294b489e5970ab16f938e26e
Roland Pangu <faruzzy@gmail.com>
2015-09-20 22:23:30 -0700

    Modified Logic

--- src/com/deitel/Run.java
+++ src/com/deitel/Run.java
@@ -3,6 +3,6 @@ package com.deitel;
 public class Run {
 	public static void main(String[] args) { 
 		Simpletron simpletron = new Simpletron();
-		simpletron.prompt();
+		simpletron.execute();
 	}
 }
