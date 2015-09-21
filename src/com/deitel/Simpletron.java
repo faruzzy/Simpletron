516d78bb0e406a4b8e0562e66e1705ff14501c64
Roland Pangu <faruzzy@gmail.com>
2015-09-20 22:23:01 -0700

    Modified logic and implemented branching

--- src/com/deitel/Simpletron.java
+++ src/com/deitel/Simpletron.java
@@ -5,47 +5,66 @@ import javax.swing.JOptionPane;
 
 public class Simpletron {
 	
-	private String[] memory = new String[100];
-	private int accumulator = 0;
-	private int instructionCounter = 0;
-	private int instructionRegister = 0;
+	private static String[] memory;
+	private static int accumulator = 0;
+	private static int instructionCounter = 0;
+	private static int instructionRegister = 0;
+	private static int globalOperand;
 
-	private void startMessage() {
-		System.out.println( "*** Welcome to Simpletron! 					***" );
+	public Simpletron() {
+		memory = new String[100];
+	}
+
+	private static void startMessage() {
+		System.out.println("*** Welcome to Simpletron! 					***");
 		System.out.println( "*** Please enter your program one instruction  ***" );
 		System.out.println( "*** (or data word) at a time. I will display   ***" );
 		System.out.println( "*** the location number and a question mark    ***" );
 		System.out.println( "*** You then type the word for that location.  ***" );
-		System.out.println( "*** Type -99999 to stop entering your program. ***" );
+		System.out.println("*** Type -99999 to stop entering your program. ***");
 	}
 
-	public void prompt() {
+	private static void prompt() {
 		startMessage();
 		Scanner input = new Scanner(System.in);
+		int counter = 0;
 
 		do {
-			System.out.printf("%d%d ? ", instructionCounter, instructionCounter);
-			memory[instructionCounter] = input.nextLine();
-
-			instructionRegister = Integer.parseInt(memory[instructionCounter]);
-			determineOperationCode( instructionRegister );
-
-			instructionCounter++;
+			System.out.printf("%02d ? ", counter, counter);
+			memory[counter] = input.nextLine();
+			counter++;
 			System.out.println();
-		} while(!(memory[instructionCounter - 1].equals("-99999")));
+		} while(!(memory[counter - 1].equals("-99999")));
 
-		System.out.println("*** Program loading Completed ***");
+		System.out.println("*** Program loading completed ***");
 		System.out.println("*** Program execution begins  ***");
 	}
 
-	private void determineOperationCode(int instructionRegister) {
+	public static void execute() {
+		prompt();
+		for (int i = 0; i < memory.length; i++) {
+			instructionCounter = i;
+			instructionRegister = Integer.parseInt(memory[instructionCounter]);
+			determineOperationCode(instructionRegister);
+
+			if (instructionRegister / 100 == 43) {
+				break;
+			}
+
+			if (globalOperand > 0) {
+				i += globalOperand - i - 1;
+			}
+		}
+	}
+
+	private static void determineOperationCode(int instructionRegister) {
 		int operationCode = instructionRegister / 100;
 		int operand = instructionRegister % 100;
 
 		switch( operationCode ) {
 			case SimpletronConstants.READ :
 				int number = 
-					Integer.parseInt( JOptionPane.showInputDialog("Please Enter the number") );
+					Integer.parseInt( JOptionPane.showInputDialog("Enter an Integer") );
 				memory[ operand ] = "" + number;
 				break;
 
@@ -75,6 +94,24 @@ public class Simpletron {
 			
 			case SimpletronConstants.MULTIPLY :
 				accumulator *= Integer.parseInt( memory[ operand ] );
+				break;
+
+			case SimpletronConstants.BRANCH:
+				globalOperand = operand;
+				break;
+
+			case SimpletronConstants.BRANCHZERO:
+				if (accumulator == 0)
+					globalOperand = operand;
+				break;
+
+			case SimpletronConstants.BRANCHNEG :
+				if (accumulator < 0)
+					globalOperand = operand;
+				break;
+
+			case SimpletronConstants.HALT:
+				System.out.println("*** Simpletron execution terminated ***");
 				break;
 		}
 	}
