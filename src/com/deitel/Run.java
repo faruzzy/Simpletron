1790725cf2b90571fbe7204e655b4fd31b1aff46
Roland Pangu <faruzzy@gmail.com>
2015-09-21 19:13:58 -0700

    Cleaned up program Entry point since it is very unlikely that we may need more than one instance of Simpletron

--- src/com/deitel/Run.java
+++ src/com/deitel/Run.java
@@ -2,7 +2,6 @@ package com.deitel;
 
 public class Run {
 	public static void main(String[] args) { 
-		Simpletron simpletron = new Simpletron();
-		simpletron.execute();
+		Simpletron.execute();
 	}
 }
